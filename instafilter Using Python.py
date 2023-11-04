abd419353095c2e36f6d1ff08ef94613ac5b2d8a
Sandesh Khadase <sandeshkhadase05@gmail.com>
2023-11-04 21:52:28 +0530

    Add files via upload

---
+++ instafilter Using Python.py
@@ -0,0 +1,19 @@
+#Fun With @Sandeshkhadase
+#Fun With @Sandeshkhadase
+#Fun With @Sandeshkhadase
+
+
+
+from instafilter import Instafilter
+
+model = Instafilter("Lo-fi")
+new_image = model("image.jpg")
+
+# To save the image, use cv2
+import cv2
+cv2.imwrite("modified_image.jpg", new_image)
+
+#Fun With @Sandeshkhadase
+#Fun With @Sandeshkhadase
+#Fun With @Sandeshkhadase
+#Fun With @Sandeshkhadase
